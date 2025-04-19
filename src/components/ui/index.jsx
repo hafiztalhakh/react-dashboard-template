7c310e8cee8df43a570e9e522ff35a7e512ad87d
Talha Khalid <hafiz.talhakh@gmail.com>
2025-04-20 01:14:07 +0500

    feat: Sidebar created

---
+++ src/components/ui/index.jsx
@@ -0,0 +1,1 @@
+export { default as FlexBetween } from "./flex_between";
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,13 +1,42 @@
+import { useState } from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import Sidebar from "@/components/layout/sidebar";
+
+const userData = {
+  _id: "1",
+  name: "Konstantine",
+  email: "kranstead0@narod.ru",
+  password: "omMDCh",
+  city: "Nurabelen",
+  state: null,
+  country: "ID",
+  occupation: "Computer Systems Analyst I",
+  phoneNumber: "8346315874",
+  transactions: [
+    "63701d74f0323986f3000158",
+    "63701d74f03239d40b00007e",
+    "63701d74f03239867500014b",
+    "63701d74f032398675000152",
+  ],
+  role: "superadmin",
+};
 
 function Layout() {
   const isDesktop = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   return (
     <Box display={isDesktop ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        user={userData}
+        isDesktop={isDesktop}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
       <Box flexGrow={1}>
-        This is a Layout
+        Navbar here
         <Outlet />
       </Box>
     </Box>
