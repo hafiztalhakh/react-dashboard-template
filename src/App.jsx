888abec09df18a79d5d06875692075fe303bfd25
Talha Khalid <hafiz.talhakh@gmail.com>
2025-04-25 11:45:09 +0500

    Admin screen created

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Overview from "@/screens/overview";
 import Daily from "@/screens/daily";
 import Monthly from "@/screens/monthly";
 import Breakdown from "@/screens/breakdown";
+import Admin from "@/screens/admin";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -34,6 +35,7 @@ function App() {
             <Route path="/daily" element={<Daily />} />
             <Route path="/monthly" element={<Monthly />} />
             <Route path="/breakdown" element={<Breakdown />} />
+            <Route path="/admin" element={<Admin />} />
           </Route>
         </Routes>
       </ThemeProvider>
--- src/components/misc/index.jsx
+++ src/components/misc/index.jsx
@@ -1,2 +1,3 @@
 export { default as StatBox } from "./stats";
 export { default as DataGridCustomToolbar } from "./data_grid_toolbar";
+export { default as CustomColumnMenu } from "./data_grid_column_menu";
