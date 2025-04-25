4163b8875c2364aacc9f19147e8fc7e994a2eee0
Talha Khalid <hafiz.talhakh@gmail.com>
2025-04-25 11:27:40 +0500

    Geography screen created

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Dashboard from "@/screens/dashboard";
 import Products from "@/screens/products";
 import Customers from "@/screens/customers";
 import Transactions from "./screens/transactions";
+import Geography from "./screens/geography";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -24,6 +25,7 @@ function App() {
             <Route path="/products" element={<Products />} />
             <Route path="/customers" element={<Customers />} />
             <Route path="/transactions" element={<Transactions />} />
+            <Route path="/geography" element={<Geography />} />
           </Route>
         </Routes>
       </ThemeProvider>
